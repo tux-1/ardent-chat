392101a0df587180f007f76ea14002a2819e56bd
Kareem Abd El-Moneam <kareemmoneeam@outlook.com>
2024-09-12 10:49:55 +0300

    Added regex_validation.dart file, added validation to login_screen.dart

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../common/constants/regex_validation.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -15,6 +17,8 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController passwordController = TextEditingController();
   bool rememberUser = false;
   bool _obscurePassword = true; // State to toggle password visibility
+
+  final _formKey = GlobalKey<FormState>(); // Add a global key for the form
 
   @override
   Widget build(BuildContext context) {
@@ -71,67 +75,69 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Widget _buildForm() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text("Welcome",
-                style: TextStyle(
-                  color: darkBlueColor,
-                  fontSize: 32,
-                  fontFamily: 'Quicksand',
-                  fontWeight: FontWeight.w700,
-                )),
-          ],
-        ),
-        _buildGreyText("Login with your information"),
-        const SizedBox(height: 20),
-        Padding(
-          padding: const EdgeInsets.only(top: 20.0, right: 20, bottom: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return Form(
+      key: _formKey, // Assign the global key to the Form widget
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              _buildGreyText("Email"),
-              _buildInputField(emailController),
-              const SizedBox(height: 50),
-              _buildGreyText("Password"),
-              _buildInputField(passwordController, isPassword: true),
-              const SizedBox(height: 35),
-              _buildLoginButton(),
-              const SizedBox(height: 25),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Didn't have an account?   ",
-                    style: TextStyle(
-                      color: darkBlueColor,
-                      fontWeight: FontWeight.w500,
-                      fontFamily: "Quicksand",
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-
-                    },
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(
-                        color: darkBlueColor,
-                        fontFamily: "Quicksand",
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  )
-                ],
-              )
+              Text("Welcome",
+                  style: TextStyle(
+                    color: darkBlueColor,
+                    fontSize: 32,
+                    fontFamily: 'Quicksand',
+                    fontWeight: FontWeight.w700,
+                  )),
             ],
           ),
-        ),
-      ],
+          _buildGreyText("Login with your information"),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, right: 20, bottom: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildGreyText("Email"),
+                _buildInputField(emailController, isPassword: false, validator: _emailValidator),
+                const SizedBox(height: 50),
+                _buildGreyText("Password"),
+                _buildInputField(passwordController, isPassword: true, validator: _passwordValidator),
+                const SizedBox(height: 35),
+                _buildLoginButton(),
+                const SizedBox(height: 25),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Didn't have an account?   ",
+                      style: TextStyle(
+                        color: darkBlueColor,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: "Quicksand",
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        // Navigate to the Sign Up screen
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: darkBlueColor,
+                          fontFamily: "Quicksand",
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -147,8 +153,8 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Widget _buildInputField(TextEditingController controller,
-      {bool isPassword = false}) {
-    return TextField(
+      {required bool isPassword, required String? Function(String?) validator}) {
+    return TextFormField(
       controller: controller,
       obscureText: isPassword ? _obscurePassword : false,
       decoration: InputDecoration(
@@ -165,14 +171,40 @@ class _LoginScreenState extends State<LoginScreen> {
         )
             : null,
       ),
+      validator: validator,
     );
+  }
+
+  String? _emailValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email';
+    }
+    if (!emailRegex.hasMatch(value)) {
+      return 'Invalid Email';
+    }
+    return null;
+  }
+
+  String? _passwordValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your password';
+    }
+    if (!passwordRegex.hasMatch(value)) {
+      return "Password must include one uppercase letter,\none lowercase letter,"
+          " one digit, one special\ncharacter (!@#\$&*~),"
+          " and be at least 8 characters.";
+    }
+    return null;
   }
 
   Widget _buildLoginButton() {
     return ElevatedButton(
       onPressed: () {
-        debugPrint("Email : ${emailController.text}");
-        debugPrint("Password : ${passwordController.text}");
+        if (_formKey.currentState?.validate() ?? false) {
+          debugPrint("Email : ${emailController.text}");
+          debugPrint("Password : ${passwordController.text}");
+          // Perform login action
+        }
       },
       style: ElevatedButton.styleFrom(
         shape: const StadiumBorder(),
