cd51062b30647920e92b4ce51c7cbe8d59162e52
ahmed <ahmedadel.2003816@gmail.com>
2024-09-27 21:23:07 +0300

    addFriendScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:ardent_chat/common/constants/routes.dart';
 
 import 'package:ardent_chat/common/theme/theme.dart';
-import 'package:ardent_chat/screens/chat_screen/cubit/chats_cubit.dart';
 import 'package:ardent_chat/screens/home_screen/home_screen.dart';
 import 'package:ardent_chat/screens/login_screen/login_screen.dart';
 import 'package:ardent_chat/screens/onboarding/onboarding_screen.dart';
@@ -10,7 +9,10 @@ import 'package:ardent_chat/screens/verification/verification_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ardent_chat/screens/chat_screen/cubit/chats_cubit.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -18,6 +20,7 @@ import 'firebase_options.dart';
 import 'common/theme/theme_provider.dart';
 
 import 'screens/signup_screen/signup.dart';
+import 'screens/add_friend_Screen/add-friend.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -49,29 +52,33 @@ class MyApp extends ConsumerWidget {
     // If Hive box value is null (not set), use the default Theme.of(context) brightness
     bool finalTheme = themePreference ?? isDarkMode;
 
+    // return MaterialApp(
     return MultiBlocProvider(
-      providers: [
+        providers: [
         BlocProvider<ChatsCubit>(create: (_) => ChatsCubit()),
-      ],
-      child: MaterialApp(
-        title: 'Ardent Chat',
-        themeMode: finalTheme ? ThemeMode.dark : ThemeMode.light,
-        theme: kLightTheme,
-        darkTheme: kDarkTheme,
-        debugShowCheckedModeBanner: false,
-        initialRoute: Routes.splashScreen,
-        routes: {
-          Routes.splashScreen: (context) => const AnimatedSplashScreen(),
-          Routes.addFriendScreen: (context) => Container(),
-          Routes.authenticationScreen: (context) => Container(),
-          Routes.loginScreen: (context) => const LoginScreen(),
-          Routes.signUpScreen: (context) => const SignUpScreen(),
-          Routes.homeScreen: (context) => const HomeScreen(),
-          Routes.onBoardingScreen: (context) => const OnboardingScreen(),
-          Routes.verifyAuthenticationScreen: (context) =>
-              const VerificationScreen(),
-        },
-      ),
+    ],
+    child: MaterialApp(
+
+    title: 'Ardent Chat',
+      themeMode: finalTheme ? ThemeMode.dark : ThemeMode.light,
+      theme: kLightTheme,
+      darkTheme: kDarkTheme,
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.splashScreen,
+      routes: {
+        Routes.splashScreen: (context) => const AnimatedSplashScreen(),
+        Routes.addFriendScreen: (context) => Container(),
+        Routes.authenticationScreen: (context) => Container(),
+        Routes.loginScreen: (context) => const LoginScreen(),
+        Routes.signUpScreen: (context) => const SignUpScreen(),
+        Routes.homeScreen: (context) => const HomeScreen(),
+        Routes.addFriendScreen:(context) => const AddFriendScreen(),
+        Routes.onBoardingScreen: (context) => const OnboardingScreen(),
+        Routes.verifyAuthenticationScreen: (context) => const VerificationScreen(),
+      },
+    )
     );
   }
 }
+
+
